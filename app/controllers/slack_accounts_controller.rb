bb16fe7fbd42de9f696c37b13b254c6a27c10ca9
Dorian Marié <dorian@dorianmarie.com>
2024-02-17 08:37:23 +0100

    add slack account controller

---
+++ app/controllers/slack_accounts_controller.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+class SlackAccountsController < ApplicationController
+  before_action :load_user
+  before_action :load_slack_account, only: %i[show edit update destroy]
+
+  def index
+    authorize SlackAccount
+
+    @slack_accounts = scope
+  end
+
+  def show
+  end
+
+  def new
+    @slack_account =
+      authorize scope.new(primary: current_user.slack_accounts.none?)
+  end
+
+  def create
+    @slack_account = authorize scope.new(slack_account_params)
+
+    if @slack_account.save
+      redirect_to @slack_account, notice: t(".notice")
+    else
+      flash.now.alert = @slack_account.alert
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @slack_account.update(slack_account_params)
+      redirect_to @slack_account, notice: t(".notice")
+    else
+      flash.now.alert = @slack_account.alert
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @slack_account.destroy!
+
+    redirect_to @slack_account.user, notice: t(".notice")
+  end
+
+  private
+
+  def load_user
+    if params[:user_id] == "me"
+      @user = current_user
+    elsif params[:user_id].present?
+      @user = policy_scope(User).find(params[:user_id])
+    end
+  end
+
+  def scope
+    if @user
+      policy_scope(SlackAccount).where(user: @user)
+    else
+      policy_scope(SlackAccount)
+    end
+  end
+
+  def id
+    params[:slack_account_id].presence || params[:id]
+  end
+
+  def load_slack_account
+    @slack_account = authorize scope.find(id)
+  end
+
+  def slack_account_params
+    if admin?
+      params.require(:slack_account).permit(
+        :user_id,
+        :primary,
+        :name,
+        :token
+      )
+    else
+      params.require(:slack_account).permit(
+        :primary,
+        :name,
+        :token
+      )
+    end
+  end
+end
