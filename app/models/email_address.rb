cd4823b9b58866ef96e81c74af15b4c03e8aad68
Dorian Marié <dorian@dorianmarie.com>
2024-02-18 22:24:30 +0100

    add missing directories

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -2,31 +2,16 @@
 
 class EmailAddress < ApplicationRecord
   EMAIL_ADDRESS_REGEXP = URI::MailTo::EMAIL_REGEXP
-  DOMAIN_REGEXP = /\A[a-zA-Z0-9]+([-.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}\z/
 
   belongs_to :user, default: -> { Current.user }
-
-  encrypts :smtp_password
 
   normalizes(
     :email_address,
     with: ->(email_address) { email_address.to_s.downcase.strip }
   )
-  normalizes(
-    :smtp_user_name,
-    with: ->(smtp_user_name) { smtp_user_name.to_s.downcase.strip }
-  )
 
   validates :email_address, presence: true
   validates :email_address, format: { with: EMAIL_ADDRESS_REGEXP }
-  validates :smtp_address, presence: true, format: { with: DOMAIN_REGEXP }
-  validates :smtp_port, presence: true
-  validates :smtp_user_name,
-            presence: true,
-            format: {
-              with: EMAIL_ADDRESS_REGEXP
-            }
-  validates :smtp_authentication, presence: true
 
   scope :primary, -> { where(primary: true) }
   scope :not_primary, -> { where(primary: false) }
@@ -47,10 +32,6 @@ class EmailAddress < ApplicationRecord
 
   def not_verified?
     !verified?
-  end
-
-  def email_address_with_display_name
-    ActionMailer::Base.email_address_with_name(email_address, display_name)
   end
 
   def deliver!(from:, to:, subject:, body:)
---
+++ app/models/smtp_account.rb
@@ -0,0 +1,52 @@
+class SmtpAccount < ApplicationRecord
+  EMAIL_ADDRESS_REGEXP = URI::MailTo::EMAIL_REGEXP
+  DOMAIN_REGEXP = /\A[a-zA-Z0-9]+([-.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}\z/
+
+  encrypts :password
+
+  belongs_to :user, default: -> { Current.user }
+
+  scope :primary, -> { where(primary: true) }
+  scope :not_primary, -> { where(primary: false) }
+  scope :verified, -> { where(verified: true) }
+  scope :not_verified, -> { where(verified: false) }
+
+  normalizes(
+    :user_name,
+    with: ->(user_name) { user_name.to_s.downcase.strip }
+  )
+
+  validates :address, presence: true, format: { with: DOMAIN_REGEXP }
+  validates :port, presence: true
+  validates :user_name,
+            presence: true,
+            format: {
+              with: EMAIL_ADDRESS_REGEXP
+            }
+  validates :password, presence: true
+  validates :authentication, presence: true
+
+  def email_address_with_display_name
+    ActionMailer::Base.email_address_with_name(user_name, display_name)
+  end
+
+  def primary?
+    !!primary
+  end
+
+  def not_primary?
+    !primary?
+  end
+
+  def verified?
+    !!verified
+  end
+
+  def not_verified?
+    !verified?
+  end
+
+  def to_s
+    user_name.presence || "SmtpAccount##{id}"
+  end
+end
