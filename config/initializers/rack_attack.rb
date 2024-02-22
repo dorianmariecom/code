d01c1819410d4657af1da05879930ac3c92a7875
Dorian Marié <dorian@dorianmarie.com>
2024-02-22 14:37:58 +0100

    rate limit sessions and verification codes

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,11 @@
 class Rack::Attack
   throttle("request/ip", limit: 300, period: 5.minutes) { |request| request.ip }
 
-  throttle("logins/ip", limit: 5, period: 20.seconds) do |request|
-    request.ip if request.path == "/session" && request.post?
+  throttle("sessions/ip", limit: 5, period: 20.seconds) do |request|
+    request.ip if request.path =~ /session/
+  end
+
+  thottle("verification_codes/ip", limit: 5, period: 20.seconds) do |request|
+    request.ip if request.path =~ /verification_code/
   end
 end
