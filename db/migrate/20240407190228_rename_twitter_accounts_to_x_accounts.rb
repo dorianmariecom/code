8db2939322adc0ddf8c7e918753de53e54e45fd1
Dorian Marié <dorian@dorianmarie.com>
2024-04-15 18:41:45 +0200

    fix error, rename

--- db/migrate/20240407190228_rename_twitter_accounts_to_x_accounts.rb
+++ db/migrate/20240407190228_rename_twitter_accounts_to_x_accounts.rb
@@ -1,4 +1,4 @@
-class RenameXAccountsToXAccounts < ActiveRecord::Migration[7.2]
+class RenameTwitterAccountsToXAccounts < ActiveRecord::Migration[7.2]
   def change
     rename_table :x_accounts, :x_accounts
   end
