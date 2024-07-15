1e1566f1b687081e0b4d05968b00687166aded58
Dorian Marié <dorian@dorianmarie.com>
2024-07-15 18:03:21 +0200

    fix solid queue error with recurring tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_07_15_134447) do
+ActiveRecord::Schema[8.0].define(version: 2024_07_15_160218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -236,6 +236,20 @@ ActiveRecord::Schema[8.0].define(version: 2024_07_15_134447) do
             name: "index_solid_queue_poll_by_queue"
   end
 
+  create_table "solid_queue_recurring_executions", force: :cascade do |t|
+    t.bigint "job_id", null: false
+    t.string "task_key", null: false
+    t.datetime "run_at", null: false
+    t.datetime "created_at", null: false
+    t.index ["job_id"],
+            name: "index_solid_queue_recurring_executions_on_job_id",
+            unique: true
+    t.index %w[task_key run_at],
+            name:
+              "index_solid_queue_recurring_executions_on_task_key_and_run_at",
+            unique: true
+  end
+
   create_table "solid_queue_scheduled_executions", force: :cascade do |t|
     t.bigint "job_id", null: false
     t.string "queue_name", null: false
@@ -314,6 +328,10 @@ ActiveRecord::Schema[8.0].define(version: 2024_07_15_134447) do
                   column: "job_id",
                   on_delete: :cascade
   add_foreign_key "solid_queue_ready_executions",
+                  "solid_queue_jobs",
+                  column: "job_id",
+                  on_delete: :cascade
+  add_foreign_key "solid_queue_recurring_executions",
                   "solid_queue_jobs",
                   column: "job_id",
                   on_delete: :cascade
