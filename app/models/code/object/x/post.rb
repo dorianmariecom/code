2a0f0c8cce51c8f2e3fa0cee237a70dd1512c0c7
Dorian Marié <dorian@dorianmarie.com>
2024-07-19 10:35:37 +0200

    html, photos and videos for x post

--- app/models/code/object/x/post.rb
+++ app/models/code/object/x/post.rb
@@ -21,6 +21,9 @@ class Code
           when "text"
             sig(args)
             code_text
+          when "html"
+            sig(args)
+            code_html
           when "retweet"
             sig(args)
             code_retweet
@@ -43,8 +46,57 @@ class Code
           if tweet
             String.new(tweet["text"])
           else
-            String.new(raw.code_get(String.new(:text)))
+            String.new(as_json["text"])
           end
+        end
+
+        def code_html
+          html = code_text.raw
+
+          mentions.each do |mention|
+            html.gsub!(
+              /( |^)(@#{mention["username"]})( |$)/,
+              '\1<a href="https://x.com/\2">\2</a>\3'
+            )
+          end
+
+          annotations.each do |annotation|
+            html.gsub!(
+              /( |^)(#{annotation["normalized_text"]})( |$)/,
+              '\1<a href="https://x.com/search?q=\2">\2</a>\3'
+            )
+          end
+
+          urls.each do |url|
+            html.gsub!(
+              /( |^)(#{url["url"]})( |$)/,
+              '\1<a href="' + url["expanded_url"] + '">' + url["expanded_url"] + '</a>\3'
+            )
+          end
+
+          cashtags.each do |cashag|
+            html.gsub!(
+              /( |^)(\$#{cashtag["tag"]})( |$)/,
+              '\1<a href="https://x.com/search?q=\2">\2</a>\3'
+            )
+          end
+
+          hashtags.each do |hashtag|
+            html.gsub!(
+              /( |^)(\$#{hashtag["tag"]})( |$)/,
+              '\1<a href="https://x.com/search?q=\2">\2</a>\3'
+            )
+          end
+
+          photos.each do |photo|
+            html += "<a href=\"#{url}\"><img src=\"#{photo["url"]}\" /></a>"
+          end
+
+          videos.each do |video|
+            html += "<a href=\"#{url}\"><img src=\"#{video["preview_image_url"]}\" /></a>"
+          end
+
+          String.new(html)
         end
 
         def code_created_at
@@ -56,7 +108,11 @@ class Code
         end
 
         def code_url
-          String.new("https://x.com/#{username}/status/#{id}")
+          String.new(url)
+        end
+
+        def url
+          "https://x.com/#{username}/status/#{id}"
         end
 
         def username
@@ -80,7 +136,7 @@ class Code
         end
 
         def author
-          includes["users"].detect { |user| user["id"].to_s == author_id.to_s }
+          users.detect { |user| user["id"] == author_id }
         end
 
         def retweet_id
@@ -95,14 +151,62 @@ class Code
           retweet ? Post.new(retweet.merge("json" => json)) : Nothing.new
         end
 
+        def entities
+          as_json["entities"] || {}
+        end
+
+        def annotations
+          entities["annotations"] || []
+        end
+
+        def mentions
+          entities["mentions"] || []
+        end
+
+        def cashtags
+          entities["cashtags"] || []
+        end
+
+        def hashtags
+          entities["hashtags"] || []
+        end
+
+        def urls
+          entities["urls"] || []
+        end
+
+        def tweets
+          includes["tweets"] || []
+        end
+
+        def media
+          includes["media"] || []
+        end
+
+        def users
+          includes["users"] || []
+        end
+
         def retweet
-          includes["tweets"].detect do |tweet|
-            tweet["id"].to_s == retweet_id.to_s
-          end
+          tweets.detect { |tweet| tweet["id"] == retweet_id }
         end
 
         def tweet
-          includes["tweets"].detect { |tweet| tweet["id"].to_s == id.to_s }
+          tweets.detect { |tweet| tweet["id"] == id }
+        end
+
+        def tweet_media
+          urls.map do |url|
+            media.detect { |media| media["media_key"] == url["media_key"] }
+          end.compact
+        end
+
+        def photos
+          tweet_media.select { |media| media["type"] == "photo" }
+        end
+
+        def videos
+          tweet_media.select { |media| media["type"] == "video" }
         end
       end
     end
