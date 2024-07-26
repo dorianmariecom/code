7a94115e032952e7fc786611e39fe96430b62301
Dorian Marié <dorian@dorianmarie.com>
2024-07-26 19:24:00 +0200

    https://github.com/rails/solid_queue/issues/267

--- app/models/program.rb
+++ app/models/program.rb
@@ -58,7 +58,7 @@ class Program < ApplicationRecord
   end
 
   def schedule!
-    scheduled_jobs.destroy_all
+    SolidQueue::Schedule.discard_all_from_jobs(scheduled_jobs)
     return unless next_at
     EvaluateAndScheduleJob.set(wait_until: next_at).perform_later(program: self)
   end
