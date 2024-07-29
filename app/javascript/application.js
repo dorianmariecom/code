d8b73d523c34d29910f4e6e6d2a7f5a4229d85f2
Dorian Marié <dorian@dorianmarie.com>
2024-07-29 20:55:27 +0200

    status bar border top on mobile

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,25 @@ import "@hotwired/turbo-rails";
 import "@hotwired/turbo";
 import "controllers";
 
+const updateBorderTopWidth = () => {
+  if (localStorage.getItem("codeStatusBarHeight")) {
+    document.body.style.borderTopWidth =
+      `${localStorage.getItem("codeStatusBarHeight")}px`;
+  }
+}
+
 window.addEventListener("message", (event) => {
   const data = JSON.parse(event.data);
 
   if (data.config) {
     document.body.classList.add("code-app", `code-app:${data.config.CODE_ENV}`);
   }
+
+  if (data.statusBarHeight) {
+    localStorage.setItem("codeStatusBarHeight", data.statusBarHeight);
+    updateBorderTopWidth()
+  }
 });
+
+window.addEventListener("load", updateBorderTopWidth)
+window.addEventListener("turbo:load", updateBorderTopWidth)
